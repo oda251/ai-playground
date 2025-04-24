e4108891f26f4c3194a33af1bc4e748b71f9cd83
oda251 <pgwyaueu@gmail.com>
2025-04-25 00:01:39 +0900

    [WIP]enable to talk

--- main.py
+++ main.py
@@ -1,28 +1,116 @@
 import flet as ft
+from google.adk.agents import Agent
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.genai import types
+import uuid
+from simple_agent.agent import build_simple_agent
+from typing import List
+
+# configuration
+APP_NAME = "simple_agent"
+session_service = InMemorySessionService()
+
+
+# util
+def create_new_session(user_id: str) -> str:
+    """Creates a new session."""
+    id = str(uuid.uuid4())
+    session_service.create_session(app_name=APP_NAME, user_id=user_id, session_id=id)
+    return id
+
+
+def delete_session(session_id: str, user_id: str):
+    """Deletes a session."""
+    session_service.delete_session(
+        app_name=APP_NAME, user_id=user_id, session_id=session_id
+    )
+
+
+def build_runnner(agent: Agent):
+    """Builds the runner with the specified agent and session service."""
+    return Runner(agent=agent, app_name=APP_NAME, session_service=session_service)
+
+
+def create_request(query: str) -> types.Content:
+    """Creates a request."""
+    return types.Content(role="user", parts=[types.Part(text=query)])
+
+
+def create_talk_card(query: str, reply: str) -> ft.Row:
+    """Creates a talk card."""
+    return ft.Card(
+        content=ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Text(value=query, weight=ft.FontWeight.BOLD, selectable=True),
+                    ft.Text(reply, selectable=True),
+                ],
+            ),
+            width=400,
+            bgcolor=ft.colors.BLUE_100,
+            padding=10,
+            border_radius=ft.border_radius.all(10),
+        )
+    )
+
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "test app"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    USER_ID = "example_user"
+    session_id = create_new_session(USER_ID)
+    query = ft.TextField(label="Query", width=300)
+    agent = build_simple_agent()
+    runner = build_runnner(agent)
+    log: List = []
+    log_view = ft.ListView(
+        col=ft.Colors.BLUE_50,
+        width=400,
+        height=400,
+    )
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
+    def on_new_session_click(e):
+        nonlocal session_id
+        nonlocal log
+        if session_id and session_id != "":
+            delete_session(session_id, USER_ID)
+        session_id = create_new_session(USER_ID)
+        log = []
         page.update()
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
+    def on_query_submit(e):
+        nonlocal log
+        if query.value == "":
+            return
+        request = create_request(query.value)
+        for event in runner.run(
+            user_id=USER_ID, session_id=session_id, new_message=request
+        ):
+            if event.is_final_response:
+                res = event.content.parts[0].text
+                log_view.controls.append(create_talk_card(query.value, res))
+                query.value = ""
+                page.update()
+                break
+        query.value = ""
         page.update()
 
     page.add(
-        ft.Row(
+        ft.Column(
             [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
+                ft.Row(
+                    [
+                        query,
+                        ft.ElevatedButton("Submit", on_click=on_query_submit),
+                        ft.ElevatedButton("New Session", on_click=on_new_session_click),
+                    ]
+                ),
+                log_view,
+            ]
         )
     )
+
 
 ft.app(main)
--- simple_agent/agent.py
+++ simple_agent/agent.py
@@ -1,6 +1,4 @@
 # @title Import necessary libraries
-import os
-import asyncio
 from google.adk.agents import Agent
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
@@ -9,35 +7,41 @@ from google.adk.runners import Runner
 from google.genai import types
 
 import warnings
+
 # Ignore all warnings
 warnings.filterwarnings("ignore")
 
 import logging
+
 logging.basicConfig(level=logging.ERROR)
 
-def build_agent():
+
+def build_simple_agent():
     """Builds the agent with the specified model and parameters."""
     return LlmAgent(
         model=LiteLlm(model="ollama_chat/gemma3"),
         name="simple_agent",
-        description=("Agent that can roll a die."),
-        instruction=("You can ask user to roll a die."),
+        description=("Agent that can chat."),
+        instruction=("You can chat with user"),
     )
+
 
 # @title Create Runner
 def build_runnner(agent: Agent, app_name: str, session_service: InMemorySessionService):
     """Builds the runner with the specified agent and session service."""
     return Runner(agent=agent, app_name=app_name, session_service=session_service)
 
+
 # @title Define Agent Interaction Function
 
-from google.genai import types # For creating message Content/Parts
+from google.genai import types  # For creating message Content/Parts
+
 
 def main():
     APP_NAME = "simple_agent"
     USER_ID = "example_user"
     SESSION_ID = "example_session"
-    agent = build_agent()
+    agent = build_simple_agent()
     session_service = InMemorySessionService()
     runner = build_runnner(agent, APP_NAME, session_service)
     # Create a session
@@ -45,16 +49,15 @@ def main():
         app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
     )
     query = "hello"
-    request = types.Content(
-        role='user',
-        parts=[types.Part(text=query)]
-    )
-    response = runner.run(
-        user_id=session.user_id,
-        session_id=session.session_id,
-        new_message=request
-    )
-    print("Response:", response[0])
+    request = types.Content(role="user", parts=[types.Part(text=query)])
+
+    for event in runner.run(
+        user_id=session.user_id, session_id=session.id, new_message=request
+    ):
+        if event.is_final_response:
+            print("Final response:", event.content.parts[0].text)
+            break
+
 
 if __name__ == "__main__":
     main()
